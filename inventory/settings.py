d92b6f82bc66f3fc63036d977e705d2dac8d431f
Sam Liu <sam@empor.com.tw>
2014-10-22 00:20:21 +0000

    update settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -91,10 +91,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../assets/')
+
 
 try:
     from local_settings import *
 except ImportError:
     pass
 
+
+STATIC_URL = '/static/' if DEBUG else '/assets/'
